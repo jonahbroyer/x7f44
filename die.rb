971061adae607d6ea65c5e9b42c0a720232a12b1
Jonah Broyer <jkbsk8er@yahoo.com>
2021-12-08 12:14:08 -0800

    Renamed prng and added a default value in the constructor/

--- die.rb
+++ die.rb
@@ -1,16 +1,16 @@
 class Die
 
-  attr_reader :number_of_sides, :random_number
+  attr_reader :number_of_sides, :random_number_generator
   attr_accessor :value
 
-  def initialize(number_of_sides)
+  def initialize(number_of_sides=6)
     @number_of_sides = number_of_sides
     @value = 1
-    @random_number = Random.new
+    @random_number_generator = Random.new
   end
 
   def roll!
-    self.value = random_number.rand(1..number_of_sides)
+    self.value = random_number_generator.rand(1..number_of_sides)
   end
 
   def makeRoll
